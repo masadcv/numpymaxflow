e46cbca3cb74f6a6760504d376ab297df664c343
masadcv <muhammad.asad@kcl.ac.uk>
2022-04-21 01:13:08 +0100

    bump to version 0.0.2rc1
    Signed-off-by: masadcv <muhammad.asad@kcl.ac.uk>

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ with open("requirements.txt", "r") as fp:
 
 setup(
     name="numpymaxflow",
-    version="0.0.1",
+    version="0.0.2rc1",
     description="numpymaxflow: Max-flow/Min-cut in Numpy for 2D images and 3D volumes",
     long_description=long_description,
     long_description_content_type="text/markdown",
